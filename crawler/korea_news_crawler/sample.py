fa23c94d353148936852d6f7d91758c8a58c3e9c
JI Kyu-Bin <50168851+jie1029@users.noreply.github.com>
2020-06-12 23:16:36 +0900

    크롤러 코드

---
+++ crawler/korea_news_crawler/sample.py
@@ -0,0 +1,7 @@
+from korea_news_crawler.articlecrawler import ArticleCrawler
+
+if __name__ == "__main__":
+    Crawler = ArticleCrawler()
+    Crawler.set_category("사회")  # 정치, 경제, 생활문화, IT과학, 사회, 세계 카테고리 사용 가능
+    Crawler.set_date_range(2020, 3, 2020, 4)
+    Crawler.start()
